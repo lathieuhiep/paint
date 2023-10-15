434209cab8034e40ee2977e4095e5b57132b53ee
LaThieuHiep <khacdiepkma90@gmail.com>
2023-10-15 22:16:39 +0700

    update code

--- includes/theme-scripts.php
+++ includes/theme-scripts.php
@@ -71,7 +71,7 @@ function paint_register_front_end(): void
   }
 
   // template discover
-  if (is_post_type_archive('paint_discover') || is_singular('paint_discover') || (is_search() && $_GET['post_type'] == 'paint_discover')) {
+  if (is_tax( 'paint_discover_cat' ) || is_post_type_archive('paint_discover') || is_singular('paint_discover') || (is_search() && $_GET['post_type'] == 'paint_discover')) {
     wp_enqueue_script('discover', get_theme_file_uri('/assets/js/discover.js'), array(), '1.0.0', true);
     wp_localize_script('discover', 'discoverAjax', array('url' => $paint_admin_url_ajax));
   }
